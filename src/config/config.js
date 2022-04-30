e83769a12f8cbbd9d2f3701497b292d4f9e3b238
Mulelur <63222158+Mulelur@users.noreply.github.com>
2022-04-30 09:59:07 +0200

    init

---
+++ src/config/config.js
@@ -0,0 +1,162 @@
+import CV from "../assets/cv/CV.pdf";
+// import GroupAss from "../assets/GROUP6ASS.pdf";
+import Profile1 from "../assets/Profile1.jpeg";
+import Profile2 from "../assets/Profile2.jpeg";
+import L from "../assets/Letter.pdf";
+
+export const ProjectDetails = {
+  projectName: "epo-e",
+  projectVersion: "1.0.0.2",
+  projectDescription: "",
+  projectAuthor: "",
+  projectAuthorEmail: "",
+  projectAuthorUrl: "",
+  projectLicense: "",
+  projectLicenseUrl: "",
+  projectCopyright: "",
+  projectHomepage: "",
+  projectRepository: "",
+  projectRepositoryType: "",
+  projectRepositoryUrl: "",
+  projectRepositoryBranch: "",
+};
+
+export const user = {
+  name: "MUDAU MURENDENI",
+  email: "mudaumurendeni@gmail.com",
+  phoneNumber: "071-978-5501",
+  address: "Tshiseni Stand no 15 Lwamondo 0985",
+  password: "",
+  confirmPassword: "",
+  role: "",
+  status: "",
+  degree: "",
+  createdAt: "",
+  updatedAt: "",
+};
+
+export const homePage = {
+  logoName: "MURENDENI",
+  intro: "I'm a future engineer",
+  profile: {
+    url: Profile1,
+    desc: "Home profile picture",
+  },
+  quote:
+    "Productivity is never an accident. It’s always the result of a commitment to excellence, intelligent planning, and focused effort.",
+  text: "I am taking new projects. Waiting to hearing about new projects, so if you'd like to chat please get in touch.",
+  links: [
+    {
+      name: "linkedin",
+      link: "",
+    },
+  ],
+};
+
+export const aboutPage = {
+  intro:
+    "I am a  Mudau Murendeni (A FUTURE ENGINEER). I am a student at vaal university of technology.",
+  names: "mudau merendeni",
+  profile: {
+    url: Profile2,
+    desc: '"I am a future engineer"',
+  },
+  summary:
+    "I AM MUDAU MURENDENI, I AM A STUDENT AT VAAL UNIVERSITY OF TECHNOLOGY . I AM DOING MECHANICAL ENGINEERING , I CHOSE TO DO THIS COURSE BECAUSE I LOVE  TO DESIGN AND BUILD SOLUTIONS TO A RANGE OF PROBLEMS.I WOULD LOVE TO BE ONE OF BEST MECHANICAL ENGINEERS IN THE WORLD WHO IMPROVES EFFICIENCY ACROSS A WIDE RANGE OF INDUSTRIES. I LOVE READING BOOKS, PLAYING NETBALL, AND GOING OUT WITH MY FRIENDS FOR A VACATION,",
+  LearningStyles: [
+    {
+      title: " Logical (Mathematical)",
+      desc: "I you like using my brain for logical and mathematical reasoning,i am a logical learner",
+    },
+    {
+      title: "Aural (Auditory-Musical)",
+      desc: "I need someone to tell me something out loud to understand it, i am, an auditory learner",
+    },
+    {
+      title: " Verbal (Linguistic)",
+      desc: "I find it easier to express myself by writing or speaking i regard it as verbal learners. i love to write and read",
+    },
+
+    {
+      title: "Physical (Kinesthetic)",
+      desc: "In this style, learning happens when the learner carries out a physical activity, rather than listening to a lecture or watching a demonstration",
+    },
+  ],
+  links: [
+    {
+      title: "Download CV",
+      link: CV,
+    },
+    {
+      title: "Download Letter",
+      link: L,
+    },
+  ],
+};
+
+export const hobbiesPage = {
+  intro: "Most of us have free time, we just don’t spend it wisely.",
+  hobbies: [
+    {
+      title: "01. Reading",
+      desc: "I READ DIFFERENT KIND OF BOOKS BUT BECAUSE AM STILL  A STUDENT WHO IS DOING A CHALLENGING COURSE I TEND TO READ MY EDUCATION BOOKS",
+    },
+    {
+      title: "02.PLAYING NETBALL",
+      desc: " I PLAY SOCCER WITH MY FRIENDS WHENEVER AM TIRED OF READING FUNNY HOW THIS TURNED INTO A HOBBY TO ME",
+    },
+    {
+      title: "03.VACATIONS",
+      desc: "I MAKE SURE EVERY MONTHS ENG I GO OUT FOR A VACATION EITHER WITH MY FRIENDS OR SIBLINGS.",
+    },
+    {
+      title: "04. SHOPPING",
+      desc: "SHOPPING IS MY THING AND I REALLY LOVE IT WHENEVER I FEEL LIKE I HAVE MONEY I JUST GO FOR A SHOPPING FUNNY HOW THIS TURNED INTO ONE OF MY HOBBIES",
+    },
+  ],
+  topImgs: [
+    {
+      url: "",
+    },
+    {
+      url: "",
+    },
+  ],
+  bottomImgs: [
+    {
+      url: "",
+    },
+    {
+      url: "",
+    },
+  ],
+};
+
+export const projectsPage = {
+  intro: "Most of us have free time, we just don’t spend it wisely.",
+  projects: [
+    {
+      title: "A new project",
+      link: "",
+      module: "Appl com",
+    },
+  ],
+  highSchoolSubjects: [
+    "Tshivenda Home Language",
+    "English first additional language",
+    "Mathematics",
+    "Life orientation",
+    "Geography",
+    "Life Sciences",
+    "Physical Sciences",
+  ],
+  modules: [
+    "Engineering Skills",
+    "Social Intelligence",
+    "Mathematics 1",
+    "Physics 1",
+    "Engineering Chemistry 1",
+    "ICT Skills",
+    "Applied Communication skills",
+  ],
+};
